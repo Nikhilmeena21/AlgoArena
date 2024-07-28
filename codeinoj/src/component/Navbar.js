4e1eaa9398fe0a3993b64db7afeeb3bbae99c6ec
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-29 00:35:55 +0530

    Update Navbar.js

--- codeinoj/src/component/Navbar.js
+++ codeinoj/src/component/Navbar.js
@@ -74,7 +74,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">CodeInOJ</Link>
+        <Link className="navbar-brand" to="/">AlgoArena</Link>
         <button
           className="navbar-toggler"
           type="button"
